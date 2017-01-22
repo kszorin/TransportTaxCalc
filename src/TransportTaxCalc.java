4a65a0b643ec30548835f113f62908a807b936ad
kszorin <kszorin@yandex.ru>
2017-01-22 19:29:51 +0700

    all

--- src/TransportTaxCalc.java
+++ src/TransportTaxCalc.java
@@ -13,33 +13,63 @@ public class TransportTaxCalc {
     private int power;
     private int year;
 
-    public TransportTaxCalc(int power, int year) {
-        this.power = power;
+    public TransportTaxCalc (int year, int power) {
         this.year = year;
+        this.power = power;
     }
 
     public float calculate() {
         Calendar cal = Calendar.getInstance();
         int currentYear = cal.get(cal.YEAR);
 
+        //мощность < 100
         if (power < 100) {
             return power*POWER_0_100;
         }
 
+        //100 < мощность < 150
         if ((power > 100) && (power <= 150)) {
             return power * POWER_100_150;
         }
 
+        //150 < мощность < 200
         if ((power > 150) && (power <= 200)) {
+            //год выпуска <= 5
             if ((currentYear - year) <=5)
                 return power * POWER_150_200[0];
+            //5 <год выпуска <= 10
             else if (((currentYear - year) > 5) && ((currentYear - year) <= 10))
                 return power * POWER_150_200[1];
+            //10 < год выпуска
             else
                 return power * POWER_150_200[2];
         }
 
+        //200 < мощность < 250
+        if ((power > 200) && (power <= 250)) {
+            //год выпуска <= 5
+            if ((currentYear - year) <=5)
+                return power * POWER_200_250[0];
+            //5 <год выпуска <= 10
+            else if (((currentYear - year) > 5) && ((currentYear - year) <= 10))
+                return power * POWER_200_250[1];
+            //10 < год выпуска
+            else
+                return power * POWER_200_250[2];
+        }
 
+        //250 < мощность
+        if (power > 250) {
+            //год выпуска <= 5
+            if ((currentYear - year) <=5)
+                return power * POWER_250[0];
+            //5 <год выпуска <= 10
+            else if (((currentYear - year) > 5) && ((currentYear - year) <= 10))
+                return power * POWER_250[1];
+            //10 < год выпуска
+            else
+                return power * POWER_250[2];
+        }
         return 0;
     }
 }
--- src/TransportTaxCalcTest.java
+++ src/TransportTaxCalcTest.java
@@ -1,8 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by kszorin on 19.01.2017.
  */
 public class TransportTaxCalcTest {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        int year, power;
 
+        System.out.print ("Введите год выпуска авто: ");
+        str = br.readLine();
+        year = Integer.parseInt(str);
+
+        System.out.print ("Введите мощность авто: ");
+        str = br.readLine();
+        power = Integer.parseInt(str);
+
+        TransportTaxCalc calc = new TransportTaxCalc(year, power);
+        System.out.printf ("Сумма налога составит %.2f руб: ", calc.calculate());
     }
 }
